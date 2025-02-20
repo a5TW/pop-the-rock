d4151c103acafcc096f2d601269a3809b2309112
a5TW <67015012+a5TW@users.noreply.github.com>
2025-02-20 14:26:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,42 @@
 input.onButtonPressed(Button.A, function () {
     sprite.change(LedSpriteProperty.X, -1)
-    x += -1
 })
 input.onPinPressed(TouchPin.P2, function () {
     sprite.change(LedSpriteProperty.Y, -1)
-    y += -1
 })
 input.onButtonPressed(Button.B, function () {
     sprite.change(LedSpriteProperty.X, 1)
-    x += 1
 })
 input.onPinPressed(TouchPin.P1, function () {
     sprite.change(LedSpriteProperty.Y, 1)
-    y += 1
 })
+let roomy = 0
+let roomx = 0
 let roomxy = ""
 let sprite: game.LedSprite = null
-led.setDisplayMode(DisplayMode.Greyscale)
 sprite = game.createSprite(2, 2)
-let x = 0
-let y = 0
-let roomx = 0
-let roomy = 0
+let bush = game.createSprite(0, 2)
 basic.forever(function () {
-    roomxy = "" + roomx + roomy
+    roomxy = "" + roomx + "," + roomy
+    if (roomxy == "0,1") {
+        bush.set(LedSpriteProperty.Brightness, 0.5)
+    } else {
+        bush.set(LedSpriteProperty.Brightness, 0)
+    }
+    if (input.pinIsPressed(TouchPin.P1) && sprite.get(LedSpriteProperty.Y) == 4) {
+        roomy += -1
+        sprite.set(LedSpriteProperty.Y, 0)
+    }
+    if (input.pinIsPressed(TouchPin.P2) && sprite.get(LedSpriteProperty.Y) == 0) {
+        roomy += 1
+        sprite.set(LedSpriteProperty.Y, 4)
+    }
+    if (input.buttonIsPressed(Button.A) && sprite.get(LedSpriteProperty.X) == 0) {
+        roomx += -1
+        sprite.set(LedSpriteProperty.X, 4)
+    }
+    if (input.buttonIsPressed(Button.B) && sprite.get(LedSpriteProperty.X) == 4) {
+        roomx += 1
+        sprite.set(LedSpriteProperty.X, 0)
+    }
 })
